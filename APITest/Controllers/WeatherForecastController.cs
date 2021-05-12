e5e90ddbe9d0f7d714bec632b811b4aa078b8fd9
Harbringer 666 <fate_90@hotmail.com>
2021-05-12 18:03:43 +0800

    eqeq

--- APITest/Controllers/WeatherForecastController.cs
+++ APITest/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace APITest.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Aloff"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "TD"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
